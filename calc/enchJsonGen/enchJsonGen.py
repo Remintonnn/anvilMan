a0cb991d4cb6d912d044a58653c09ab68058a133
Reminton <a1187731600@gmail.com>
2026-07-29 00:27:27 +0800

    add helper functions to Enchantment class add Id attribute to Enchantment class add MultiplierItem and renamed Multiplier to MultiplierBook fix "compatable" and "giantify" spelling add "other" to EnchantableItems

--- calc/enchJsonGen/enchJsonGen.py
+++ calc/enchJsonGen/enchJsonGen.py
@@ -42,7 +42,12 @@ class comp(Enum):
     trident = "Trident"
     fishingRod = "FishingRod"
     shield = "Shield"
+    # These 2 are kinda confusing, I'm confused as well
+    # the tag all on the enchantment means it compatibles with ALL enchantable items
+    # but it will mean compatible with EVERY enchantment if as an item
+    # the tag other is an item only tag and means it only compatible with ench with tag all
     all = "All"
+    other = "Other" # should not be used on any enchantment
 class ench(StrEnum):
     """Enchantment names"""
     # Vanilla
@@ -104,7 +109,7 @@ class ench(StrEnum):
     extension="extension"
     spear="spear"
     miniaturize="miniaturize"
-    giantify="giantify"
+    gigantify="giantify"
     nimble="nimble"
     frozen="frozen"
     delicacy="delicacy"
@@ -125,15 +130,17 @@ class enchTags(StrEnum):
     vanilla="vanilla"
     catBud="catBud"
 
-def EnchantmentObjectMaker(ID:Enum,CompatibleItems:list|Enum,Muitiplier:int,MaxLevel:int,Names:list[str],Tags:None):
+def EnchantmentObjectMaker(ID:Enum,CompatibleItems:list|Enum,MaxLevel:int,MultiplierBook:int,MultiplierItem:int,Names:list[str],Tags:None):
     global TheBigDict
     if Tags is None: Tags = []
     if isinstance(CompatibleItems,Enum): CompatibleItems = CompatibleItems.value
     if not isinstance(CompatibleItems,list): CompatibleItems = [CompatibleItems]
     if not isinstance(Names,list): Names = [Names]
     dictThing = {
+        "Id":ID.value,
         "CompatibleItems":CompatibleItems,
-        "Muitiplier":Muitiplier,
+        "MultiplierBook":MultiplierBook,
+        "MultiplierItem":MultiplierItem,
         "MaxLevel":MaxLevel,
         "MutexEnch":[],
         "Tags":Tags,
@@ -142,86 +149,86 @@ def EnchantmentObjectMaker(ID:Enum,CompatibleItems:list|Enum,Muitiplier:int,MaxL
     TheBigDict[ID]=dictThing
 
 def VanillaEnch():
-    def EOM(Name, CompatibleItems, Multiplier, MaxLevel, Names):
-        return EnchantmentObjectMaker(Name, CompatibleItems, Multiplier, MaxLevel, Names, [enchTags.vanilla])
+    def EOM(ID:Enum,CompatibleItems:list|Enum,MaxLevel:int,MultiplierBook:int,MultiplierItem:int,Names:list[str]):
+        return EnchantmentObjectMaker(ID, CompatibleItems, MaxLevel, MultiplierBook, MultiplierItem, Names, [enchTags.vanilla])
     # Ordered via ID, see https://minecraft.wiki/w/Anvil_mechanics#Costs_for_combining_enchantments
-    EOM(ench.protection,comp.armors,1,4,["保護","Protection"])
-    EOM(ench.fireProtection,comp.armors,1,4,["火焰保護","Fire Protection"])
-    EOM(ench.featherFalling,comp.shoes,1,4,["輕盈","Feather Falling"])
-    EOM(ench.blastProtection,comp.armors,2,4,["爆炸保護","Blast Protection"])
-    EOM(ench.projectileProtection,comp.armors,1,4,["投射物保護","Projectile Protection"])
-    EOM(ench.thorns,comp.armors,4,3,["尖刺","Thorns"])
-    EOM(ench.respiration,comp.hat,2,3,["水中呼吸","Respiration"])
-    EOM(ench.depthStrider,comp.shoes,2,3,["深海漫遊","Depth Strider"])
-    EOM(ench.aquaAffinity,comp.hat,2,1,["親水性","Aqua Affinity"])
-    EOM(ench.sharpness,comp.meleeSAS,1,5,["鋒利","Sharpness"])
-    EOM(ench.smite,comp.meleeSAMS,1,5,["不死剋星","Smite"])
-    EOM(ench.baneOfArthropods,comp.meleeSAMS,1,5,["節肢剋星","Bane of Arthropods"])
-    EOM(ench.knockback,comp.meleeSS,1,2,["擊退","Knockback"])
-    EOM(ench.fireAspect,comp.meleeSMS,2,2,["燃燒","Fire Aspect"])
-    EOM(ench.looting,comp.meleeSS,2,3,["掠奪","Looting"])
-    EOM(ench.efficiency,comp.miningToolPASSH,1,5,["效率","Efficiency"])
-    EOM(ench.silkTouch,comp.miningToolPASH,4,1,["絲綢之觸","Silk Touch"])
-    EOM(ench.unbreaking,comp.all,1,3,["耐久","Unbreaking"])
-    EOM(ench.fortune,comp.miningToolPASH,2,3,["幸運","Fortune"])
-    EOM(ench.power,comp.bow,1,5,["強力","Power"])
-    EOM(ench.punch,comp.bow,2,2,["衝擊","Punch"])
-    EOM(ench.flame,comp.bow,2,1,["火焰","Flame"])
-    EOM(ench.infinity,comp.bow,4,1,["無限","Infinity"])
-    EOM(ench.luckOfTheSea,comp.fishingRod,2,3,["海洋的祝福","Luck of the Sea"])
-    EOM(ench.lure,comp.fishingRod,2,3,["魚餌","Lure"])
-    EOM(ench.frostWalker,comp.shoes,2,2,["冰霜行者","Frost Walker"])
-    EOM(ench.mending,comp.all,2,1,["修補","Mending"])
-    EOM(ench.curseOfBinding,comp.armorsAndWing,4,1,["綁定詛咒","Curse of Binding"])
-    EOM(ench.curseOfVanishing,comp.all,4,1,["消失詛咒","Curse of Vanishing"])
-    EOM(ench.impaling,comp.trident,2,5,["魚叉","Impaling"])
-    EOM(ench.riptide,comp.trident,2,3,["波濤","Riptide"])
-    EOM(ench.loyalty,comp.trident,1,3,["忠誠","Loyalty"])
-    EOM(ench.channeling,comp.trident,4,1,["喚雷","Channeling"])
-    EOM(ench.multishot,comp.crossbow,2,1,["分裂箭矢","Multishot"])
-    EOM(ench.piercing,comp.crossbow,1,4,["貫穿","Piercing"])
-    EOM(ench.quickCharge,comp.crossbow,1,3,["快速上弦","Quick Charge"])
-    EOM(ench.soulSpeed,comp.shoes,4,3,["靈魂疾走","Soul Speed"])
-    EOM(ench.swiftSneak,comp.pants,4,3,["迅捷潛行","Swift Sneak"])
-    EOM(ench.windBurst,comp.mase,2,3,["風爆","Wind Burst"])
-    EOM(ench.density,comp.mase,1,5,["緻密","Density"])
-    EOM(ench.breach,comp.mase,2,4,["破甲","Breach"])
-    EOM(ench.lunge,comp.spear,1,3,["突刺","Lunge"])
-    EOM(ench.sweepingEdge,comp.sword,2,3,["橫掃之刃","Sweeping Edge"])
+    EOM(ench.protection,comp.armors,4,1,1,["保護","Protection"])
+    EOM(ench.fireProtection,comp.armors,4,1,2,["火焰保護","Fire Protection"])
+    EOM(ench.featherFalling,comp.shoes,4,1,2,["輕盈","Feather Falling"])
+    EOM(ench.blastProtection,comp.armors,4,2,4,["爆炸保護","Blast Protection"])
+    EOM(ench.projectileProtection,comp.armors,4,1,2,["投射物保護","Projectile Protection"])
+    EOM(ench.thorns,comp.armors,3,4,8,["尖刺","Thorns"])
+    EOM(ench.respiration,comp.hat,3,2,4,["水中呼吸","Respiration"])
+    EOM(ench.depthStrider,comp.shoes,3,2,4,["深海漫遊","Depth Strider"])
+    EOM(ench.aquaAffinity,comp.hat,1,2,4,["親水性","Aqua Affinity"])
+    EOM(ench.sharpness,comp.meleeSAS,5,1,1,["鋒利","Sharpness"])
+    EOM(ench.smite,comp.meleeSAMS,5,1,2,["不死剋星","Smite"])
+    EOM(ench.baneOfArthropods,comp.meleeSAMS,5,1,2,["節肢剋星","Bane of Arthropods"])
+    EOM(ench.knockback,comp.meleeSS,2,1,2,["擊退","Knockback"])
+    EOM(ench.fireAspect,comp.meleeSMS,2,2,4,["燃燒","Fire Aspect"])
+    EOM(ench.looting,comp.meleeSS,3,2,4,["掠奪","Looting"])
+    EOM(ench.efficiency,comp.miningToolPASSH,5,1,1,["效率","Efficiency"])
+    EOM(ench.silkTouch,comp.miningToolPASH,1,4,8,["絲綢之觸","Silk Touch"])
+    EOM(ench.unbreaking,comp.all,3,1,2,["耐久","Unbreaking"])
+    EOM(ench.fortune,comp.miningToolPASH,3,2,4,["幸運","Fortune"])
+    EOM(ench.power,comp.bow,5,1,1,["強力","Power"])
+    EOM(ench.punch,comp.bow,2,2,4,["衝擊","Punch"])
+    EOM(ench.flame,comp.bow,1,2,4,["火焰","Flame"])
+    EOM(ench.infinity,comp.bow,1,4,8,["無限","Infinity"])
+    EOM(ench.luckOfTheSea,comp.fishingRod,3,2,4,["海洋的祝福","Luck of the Sea"])
+    EOM(ench.lure,comp.fishingRod,3,2,4,["魚餌","Lure"])
+    EOM(ench.frostWalker,comp.shoes,2,2,4,["冰霜行者","Frost Walker"])
+    EOM(ench.mending,comp.all,1,2,4,["修補","Mending"])
+    EOM(ench.curseOfBinding,comp.armorsAndWing,1,4,8,["綁定詛咒","Curse of Binding"])
+    EOM(ench.curseOfVanishing,comp.all,1,4,8,["消失詛咒","Curse of Vanishing"])
+    EOM(ench.impaling,comp.trident,5,2,4,["魚叉","Impaling"])
+    EOM(ench.riptide,comp.trident,3,2,4,["波濤","Riptide"])
+    EOM(ench.loyalty,comp.trident,3,1,1,["忠誠","Loyalty"])
+    EOM(ench.channeling,comp.trident,1,4,8,["喚雷","Channeling"])
+    EOM(ench.multishot,comp.crossbow,1,2,4,["分裂箭矢","Multishot"])
+    EOM(ench.piercing,comp.crossbow,4,1,1,["貫穿","Piercing"])
+    EOM(ench.quickCharge,comp.crossbow,3,1,2,["快速上弦","Quick Charge"])
+    EOM(ench.soulSpeed,comp.shoes,3,4,8,["靈魂疾走","Soul Speed"])
+    EOM(ench.swiftSneak,comp.pants,3,4,8,["迅捷潛行","Swift Sneak"])
+    EOM(ench.windBurst,comp.mase,3,2,4,["風爆","Wind Burst"])
+    EOM(ench.density,comp.mase,5,1,2,["緻密","Density"])
+    EOM(ench.breach,comp.mase,4,2,4,["破甲","Breach"])
+    EOM(ench.lunge,comp.spear,3,1,2,["突刺","Lunge"])
+    EOM(ench.sweepingEdge,comp.sword,3,2,4,["橫掃之刃","Sweeping Edge"])
 def CatBudEnch():
-    def EOM(Name, CompatibleItems, Multiplier, MaxLevel, Names):
-        return EnchantmentObjectMaker(Name, CompatibleItems, Multiplier, MaxLevel, Names, [enchTags.catBud])
-    EOM(ench.voiceKizunaAkari,comp.hat,1,1,["動作音效 紲星燈","Voice Kizuna Akari"])
-    EOM(ench.voiceYuzukiYukari,comp.hat,1,1,["動作音效 結月緣","Voice Yuzuki Yukari"])
-    EOM(ench.voiceKotonohaAkane,comp.hat,1,1,["動作音效 琴葉茜","Voice Kotonoha Akane"])
-    EOM(ench.voiceKotonohaAoi,comp.hat,1,1,["動作音效 琴葉葵","Voice Kotonoha Aoi"])
-    EOM(ench.autoRepair,comp.all,1,3,["自動修補","Auto Repair"])
-    EOM(ench.criticalChance,comp.axe,1,4,["爆擊可能","Critical Chance"])
-    EOM(ench.oreSmelting,comp.pickaxe,4,1,["自動熔煉","Ore Smelting"])
-    EOM(ench.vampiric,comp.meleeSS,1,4,["吸血","Vampiric"])
-    EOM(ench.healthy,comp.cloth,1,3,["健康","Healthy"])
-    EOM(ench.gale,comp.wing,1,3,["疾風","Gale"])
-    EOM(ench.breakRecovery,comp.all,1,1,["毀損救援","Break Recovery"])
-    EOM(ench.extension,comp.miningToolPASSH,1,3,["延伸","Extension"])
-    EOM(ench.spear,comp.axe,1,3,["長茅","Spear"])
-    EOM(ench.miniaturize,comp.armors,1,1,["迷你化","Miniaturize"])
-    EOM(ench.giantify,comp.armors,1,2,["巨大化","Giantify"])
-    EOM(ench.nimble,comp.wing,1,3,["靈巧","Nimble"])
-    EOM(ench.frozen,comp.sword,2,3,["冰凍","Frozen"])
-    EOM(ench.delicacy,comp.hat,1,3,["美食","Delicacy"])
-    EOM(ench.steadySneak,comp.pants,4,3,["穩重潛行","Steady Sneak"])
-    EOM(ench.wisdom,comp.meleeSS,1,5,["智慧","Wisdom"])
-    EOM(ench.sacrificeExperience,comp.armors,4,1,["獻祭經驗","Sacrifice Experience"])
-    EOM(ench.knockbackResistance,comp.armors,1,2,["擊退保護","Knockback Resistance"])
-    EOM(ench.buoyancy,comp.wing,2,3,["浮力","Buoyancy"])
-    EOM(ench.gravity,comp.cloth,2,3,["重力","Gravity"])
-    EOM(ench.stubborn,comp.cloth,1,4,["掘強","Stubborn"])
-    EOM(ench.fragile,comp.cloth,1,3,["脆弱","Fragile"])
-    EOM(ench.windBlast,comp.shield,2,5,["風擊","Wind Blast"])
-    EOM(ench.eternal,comp.all,1,1,["不滅","Eternal"])
-    EOM(ench.overclocking,comp.miningToolPASSH,1,5,["超頻","Overclocking"])
-    EOM(ench.lightSpeed,comp.shoes,4,3,["燭光疾走","Light Speed"])
-    EOM(ench.infiniteDurability,comp.all,1,1,["無限耐久","Infinite Durability"])
+    def EOM(ID:Enum,CompatibleItems:list|Enum,MaxLevel:int,MultiplierBook:int,MultiplierItem:int,Names:list[str]):
+        return EnchantmentObjectMaker(ID, CompatibleItems, MaxLevel, MultiplierBook, MultiplierItem, Names, [enchTags.catBud])
+    EOM(ench.voiceKizunaAkari,comp.hat,1,1,1,["動作音效 紲星燈","Voice Kizuna Akari"])
+    EOM(ench.voiceYuzukiYukari,comp.hat,1,1,1,["動作音效 結月緣","Voice Yuzuki Yukari"])
+    EOM(ench.voiceKotonohaAkane,comp.hat,1,1,1,["動作音效 琴葉茜","Voice Kotonoha Akane"])
+    EOM(ench.voiceKotonohaAoi,comp.hat,1,1,1,["動作音效 琴葉葵","Voice Kotonoha Aoi"])
+    EOM(ench.autoRepair,comp.all,3,1,1,["自動修補","Auto Repair"])
+    EOM(ench.criticalChance,comp.axe,4,1,1,["爆擊可能","Critical Chance"])
+    EOM(ench.oreSmelting,comp.pickaxe,1,4,8,["自動熔煉","Ore Smelting"])
+    EOM(ench.vampiric,comp.meleeSS,4,1,1,["吸血","Vampiric"])
+    EOM(ench.healthy,comp.cloth,3,1,1,["健康","Healthy"])
+    EOM(ench.gale,comp.wing,3,1,1,["疾風","Gale"])
+    EOM(ench.breakRecovery,comp.all,1,1,1,["毀損救援","Break Recovery"])
+    EOM(ench.extension,comp.miningToolPASSH,3,1,1,["延伸","Extension"])
+    EOM(ench.spear,comp.axe,3,1,1,["長茅","Spear"])
+    EOM(ench.miniaturize,comp.armors,1,1,1,["迷你化","Miniaturize"])
+    EOM(ench.gigantify,comp.armors,2,1,1,["巨大化","Gigantify"])
+    EOM(ench.nimble,comp.wing,3,1,1,["靈巧","Nimble"])
+    EOM(ench.frozen,comp.sword,3,2,4,["冰凍","Frozen"])
+    EOM(ench.delicacy,comp.hat,3,1,1,["美食","Delicacy"])
+    EOM(ench.steadySneak,comp.pants,3,4,8,["穩重潛行","Steady Sneak"])
+    EOM(ench.wisdom,comp.meleeSS,5,1,1,["智慧","Wisdom"])
+    EOM(ench.sacrificeExperience,comp.armors,1,4,8,["獻祭經驗","Sacrifice Experience"])
+    EOM(ench.knockbackResistance,comp.armors,2,1,1,["擊退保護","Knockback Resistance"])
+    EOM(ench.buoyancy,comp.wing,3,2,4,["浮力","Buoyancy"])
+    EOM(ench.gravity,comp.cloth,3,2,4,["重力","Gravity"])
+    EOM(ench.stubborn,comp.cloth,4,1,2,["掘強","Stubborn"])
+    EOM(ench.fragile,comp.cloth,3,1,1,["脆弱","Fragile"])
+    EOM(ench.windBlast,comp.shield,5,2,2,["風擊","Wind Blast"])
+    EOM(ench.eternal,comp.all,1,1,1,["不滅","Eternal"])
+    EOM(ench.overclocking,comp.miningToolPASSH,5,1,2,["超頻","Overclocking"])
+    EOM(ench.lightSpeed,comp.shoes,3,4,8,["燭光疾走","Light Speed"])
+    EOM(ench.infiniteDurability,comp.all,1,4,8,["無限耐久","Infinite Durability"])
 
 def mutexEnchs():
     # See Trident or Dur bar is a lie for more complex mutex relations
@@ -295,7 +302,7 @@ def mutexEnchs():
         ],[
             # Big or smol
             ench.miniaturize,
-            ench.giantify
+            ench.gigantify
         ],[
             # Fire of ice
             ench.frozen,
--- calc/enchantments.py
+++ calc/enchantments.py
@@ -1,4 +1,5 @@
 import json
+from enum import Enum
 from calc.enchJsonGen.enchJsonGen import ench as EnchantmentId
 from calc.enchJsonGen.enchJsonGen import enchTags as EnchantmentTags
 from calc.enchJsonGen.enchJsonGen import comp as EnchantableItems
@@ -13,9 +14,11 @@ class EnchantmentsClass:
             totalEnch = json.load(jsonFile)
             for id, content in totalEnch.items():
                 ench = Enchantment(
+                    content["Id"],
                     content["CompatibleItems"],
-                    content["Muitiplier"],
                     content["MaxLevel"],
+                    content["MultiplierBook"],
+                    content["MultiplierItem"],
                     content["MutexEnch"],
                     content["Tags"],
                     content["Names"]
@@ -23,13 +26,22 @@ class EnchantmentsClass:
                 self.dict[id] = ench
 
 class Enchantment:
-    def __init__(self,comp,multiplier,maxlvl,mutex,tags,names):
-        self.compatableItems:list[str] = comp
-        self.multiplier:int = multiplier
+    def __init__(self,Id,comp,maxlvl,mulBook,mulItem,mutex,tags,names):
+        self.Id = Id
+        self.compatibleItems:list[str] = comp
         self.maxlvl:int = maxlvl
+        self.multiplierBook:int = mulBook
+        self.multiplierItem:int = mulItem
         self.mutexEnch:list[str] = mutex
         self.tags:list[str] = tags
         self.names:list[str] = names
+    def isCompatibleWith(self,item:str|Enum):
+        if isinstance(item,Enum): item = item.value
+        if item == EnchantableItems.all.value: return True
+        if item == EnchantableItems.other.value: return EnchantableItems.all.value in self.compatibleItems
+        return item in self.compatibleItems
+    def conflictsWith(self,ench:"Enchantment"):
+        return ench.Id in self.mutexEnch
 
 enchantments = EnchantmentsClass()
 # ==================
