93e3eaf165100800162f17617ffee3e3ab4e01d6
Reminton <a1187731600@gmail.com>
2026-08-09 17:18:52 +0800

    optimized Enchantment.conflictsWith()

--- calc/enchantments.py
+++ calc/enchantments.py
@@ -1,8 +1,11 @@
 import json
+from dataclasses import dataclass
 from enum import Enum
 from calc.enchJsonGen.enchJsonGen import ench as EnchantmentId
 from calc.enchJsonGen.enchJsonGen import enchTags as EnchantmentTags
 from calc.enchJsonGen.enchJsonGen import comp as EnchantableItems
+
+THE_ENCH_CONFLICT_MAP = []
 
 class EnchantmentsClass:
     def __getitem__(self, key:str):
@@ -10,10 +13,12 @@ class EnchantmentsClass:
     def __init__(self):
         ENCH_JSON = r"calc/enchJsonGen/Ench.json"
         self.dict:dict[str,Enchantment] = {}
+        self.totalEnchantments = 0
         with open(ENCH_JSON,"r",encoding="UTF-8") as jsonFile:
             totalEnch = json.load(jsonFile)
             for id, content in totalEnch.items():
                 ench = Enchantment(
+                    self.totalEnchantments,
                     content["Id"],
                     content["CompatibleItems"],
                     content["MaxLevel"],
@@ -24,24 +29,26 @@ class EnchantmentsClass:
                     content["Names"]
                 )
                 self.dict[id] = ench
+                self.totalEnchantments += 1
 
+@dataclass(frozen=True,slots=True)
 class Enchantment:
-    """You really shouldn't change the content of this"""
-    def __init__(self,Id,comp,maxlvl,mulBook,mulItem,mutex,tags,names):
-        self.Id = Id
-        self.compatibleItems:list[str] = comp
-        self.maxlvl:int = maxlvl
-        self.multiplierBook:int = mulBook
-        self.multiplierItem:int = mulItem
-        self.mutexEnch:list[str] = mutex
-        self.tags:list[str] = tags
-        self.names:list[str] = names
+    Id:int # only used for computing
+    IdStr:str
+    compatibleItems:list[str]
+    maxlvl:int
+    multiplierBook:int
+    multiplierItem:int
+    mutexEnch:list[str]
+    tags:list[str]
+    names:list[str]
     def isCompatibleWith(self,item:str|Enum):
+        """Not optimized, please cache the result"""
         if isinstance(item,Enum): item = item.value
         if EnchantableItems.all.value in self.compatibleItems: return True
         return item in self.compatibleItems
     def conflictsWith(self,ench:"Enchantment"):
-        return ench.Id in self.mutexEnch
+        return THE_ENCH_CONFLICT_MAP[self.Id] & (1<<ench.Id)
     def __str__(self):
         return self.names[0]
     def __repr__(self):
@@ -50,13 +57,12 @@ class Enchantment:
         if not isinstance(otherEnch,Enchantment): return NotImplemented
         return self.Id==otherEnch.Id
     def __hash__(self):
-        return hash(self.Id)
+        return self.Id
 
 enchantments = EnchantmentsClass()
-# ==================
-
-# tot = Enchantments()
-# for k in tot.dict.keys():
-#     e = tot.dict[k]
-#     print(tot.dict[k].names)
-# print(EnchantmentId.protection)
+THE_ENCH_CONFLICT_MAP = [0]*enchantments.totalEnchantments
+IdStr2Id={ench.IdStr:ench.Id for ench in enchantments.dict.values()}
+for enchA in enchantments.dict.values():
+    THE_ENCH_CONFLICT_MAP.append(0)
+    for enchB in enchA.mutexEnch:
+        THE_ENCH_CONFLICT_MAP[enchA.Id] |= 1 << IdStr2Id[enchB]
